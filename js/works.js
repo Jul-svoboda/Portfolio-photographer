d9250894fcb2303e60c433988f4653579e448331
Jul-svoboda <july.svoboda@gmail.com>
2026-03-13 22:03:23 +0300

    feat: adding animation of scrolling gallery of works in the portfolio block

---
+++ js/works.js
@@ -0,0 +1,24 @@
+const works = document.querySelector('.portfolio__gallery');
+let speed = 0;
+window.addEventListener('load', () => {
+    works.scrollLeft = (works.scrollWidth - works.clientWidth) / 2;
+});
+works.addEventListener('mouseover', function(event){
+    const rect = works.getBoundingClientRect();
+    let horizontalCoordinate = event.clientX - rect.left;
+    let percent = horizontalCoordinate/rect.width;
+    if (percent < 0.3) {
+        speed = -(0.3-percent)*20;
+    } else if (percent > 0.7) {
+        speed = (percent-0.7)*20;
+    } else {speed = 0};
+})
+works.addEventListener('mouseleave', function(){
+    speed = 0;
+})
+
+function animation() {
+    works.scrollLeft = works.scrollLeft + speed;
+    requestAnimationFrame(animation);
+}
+animation();
