a83445a7f394d9e98db840c48488b8726fa44d78
Jul-svoboda <july.svoboda@gmail.com>
2026-03-18 19:11:15 +0300

    feat: implemented saving the accordion state when the page is reloaded

--- js/accordion.js
+++ js/accordion.js
@@ -8,6 +8,10 @@ accordion.addEventListener('click', function(event){
     let verticalLine = accordionItem.querySelector('.accordion__icon--vertical');
     let answer = accordionItem.querySelector('.accordion__answer');
 
+    
+    let items = [...accordion.querySelectorAll('.accordion__item')];
+    let index = items.indexOf(accordionItem);
+
     const isActive = question.classList.contains('active');
     
     for (let item of accordion.querySelectorAll('.accordion__question')){
@@ -25,5 +29,29 @@ accordion.addEventListener('click', function(event){
         question.classList.toggle('active');
         verticalLine.classList.toggle('active');
         answer.classList.toggle('active');
+        localStorage.setItem('accordion', index);
+    } else {
+        localStorage.removeItem('accordion');
     }
 })
+
+window.addEventListener('load', () => {
+    const items = document.querySelectorAll('.accordion__item');
+    const savedIndex = localStorage.getItem('accordion');
+
+    let indexToOpen;
+
+    if (savedIndex !== null) {
+        indexToOpen = savedIndex;
+    } else {
+        indexToOpen = 0;
+    }
+
+    const item = items[indexToOpen];
+    if (!item) return;
+
+    item.querySelector('.accordion__question').classList.add('active');
+    item.querySelector('.accordion__icon--vertical').classList.add('active');
+    item.querySelector('.accordion__answer').classList.add('active');
+});
+
